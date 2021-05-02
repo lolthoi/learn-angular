be1aaefb374d8c5ba190d006e0b19c7a3b7cb8ce
Dương Hoàng <manchestervn1996@gmail.com>
2021-05-02 23:01:27 +0700

    admin-site

--- demoapp/src/app/app.module.ts
+++ demoapp/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SidebarComponent, HeaderComponent, ContentComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
